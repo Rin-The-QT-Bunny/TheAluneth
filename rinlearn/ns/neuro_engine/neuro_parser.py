d2bc71a22c9d00db74c29d878ec39c5c70c03d47
Melkor <rintfd@163.com>
2022-07-19 09:13:56 +0800

    update

--- rinlearn/ns/neuro_engine/neuro_parser.py
+++ rinlearn/ns/neuro_engine/neuro_parser.py
@@ -8,8 +8,8 @@ class NeuroDecoder(nn.Module):
     def __init__(self,inputs_dim,DSL):
         super().__init__()
         self.inputs_dim = inputs_dim
-        self.projection_operator = FCBlock(512,3,inputs_dim,DSL.op_dim)
-        self.repeater = FCBlock(512,3,DSL.op_dim + inputs_dim,inputs_dim)
+        self.projection_operator = FCBlock(256,2,inputs_dim,DSL.op_dim)
+        self.repeater = FCBlock(256,2,DSL.op_dim + inputs_dim,inputs_dim)
         self.DSL = DSL
         self.working_loss = 0
         self.counter = 0
@@ -34,7 +34,7 @@ class NeuroDecoder(nn.Module):
         #key_proj = key_proj/torch.norm(key_proj)
         #print("key proj:",key_proj)
         #pdf = torch.softmax(torch.matmul(key_proj,operator_features.permute(1,0)),1)
-        pdf = torch.sigmoid( (torch.cosine_similarity(key_proj,operator_features)-0.2) /0.1)
+        pdf = torch.sigmoid( torch.cosine_similarity(key_proj,operator_features) /0.125)
         pdf = pdf/torch.sum(pdf)
         return operator_keys,pdf,require_args
 
@@ -76,6 +76,88 @@ class NeuroDecoder(nn.Module):
         if (not track):
             return root,self.working_loss
         return root,self.working_loss,traj
+
+
+class NeuroAttnDecoder(nn.Module):
+    def __init__(self,inputs_dim,DSL):
+        super().__init__()
+        self.inputs_dim = inputs_dim
+        self.projection_operator = FCBlock(256,4,inputs_dim,DSL.op_dim)
+        self.repeater = FCBlock(256,4,DSL.op_dim + inputs_dim,inputs_dim)
+        self.DSL = DSL
+        self.working_loss = 0
+        self.counter = 0
+
+    def operator_pdf(self,key,vecs,Type):
+        valid_operators = self.DSL.operators_of_type(Type)
+        valid_constants = self.DSL.constants_of_type(Type)
+        possible_operators = valid_operators
+        possible_operators.extend(valid_constants)
+
+        operator_features = []
+        operator_keys = []
+        require_args = []
+        for op in possible_operators:
+            operator_features.append(op.semantics_reprs())
+            operator_keys.append(op.token)
+            if (op.__class__.__name__ == "Operator"):
+                require_args.append(True)
+            else:require_args.append(False)
+        operator_features = torch.cat(operator_features,0)
+        key_proj = self.projection_operator(key) 
+
+        vecs = torch.tensor(vecs)
+        #print(key_proj.shape,vecs.shape)
+
+        scores = torch.sigmoid(torch.cosine_similarity(key_proj , vecs) * 7).unsqueeze(-1)
+        #print(scores.shape,vecs.shape)
+        key = torch.sum(scores * vecs,0)
+        #key_proj = key_proj/torch.norm(key_proj)
+        #print("key proj:",key_proj)
+        #pdf = torch.softmax(torch.matmul(key_proj,operator_features.permute(1,0)),1)
+        pdf = torch.sigmoid( (torch.cosine_similarity(key,operator_features)-0.2) /0.1)
+        pdf = pdf/torch.sum(pdf)
+        return operator_keys,pdf,require_args
+
+    def forward(self,semantics,vectors,target_tree = None,track = False):
+        flag = True
+        if (target_tree != None):
+            flag = False
+        self.working_loss = 0
+        self.counter = 0
+        root = FuncNode("Root")
+        traj = []
+        def parse(head,semantics_key,Type):
+            if(track):
+                traj.append(self.projection_operator(semantics))
+            keys,pdf,args_info = self.operator_pdf(semantics_key,vectors,Type)
+            
+            if (flag):
+                max_index = torch.argmax(pdf)
+            else:
+                max_index = keys.index(target_tree[self.counter])
+                self.counter += 1
+            current_operator_name = keys[max_index]
+
+            current_node = FuncNode(current_operator_name) # create the operator node
+            current_node.function = args_info[max_index]
+            head.children.append(current_node) # attach the node created
+            self.working_loss += torch.log(pdf[max_index])
+            # check whether there is a branching by the DSL grammar
+            if ( args_info[max_index]):
+                # start the branching sequence
+                args_types,args_features = self.DSL.get_operator_args(current_operator_name)
+                for i in range(len(args_types)):
+                    arg = args_types[i]
+                    #print(i,arg)
+                    new_key = self.repeater(torch.cat([args_features[i:i+1],semantics_key],-1))
+                    parse(current_node,new_key,arg)
+            return 0,self.working_loss
+        parse(root,semantics,"All")
+        if (not track):
+            return root,self.working_loss
+        return root,self.working_loss,traj
+
 
 class Reaver(nn.Module):
     def __init__(self,config,DSL):
