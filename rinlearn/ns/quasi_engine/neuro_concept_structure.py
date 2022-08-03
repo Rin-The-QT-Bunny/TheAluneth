b208f549412056ce5b8d0a5587de2381c7953aa6
Melkor <rintfd@163.com>
2022-08-03 22:11:13 +0800

    Update neuro_concept_structure.py

---
+++ rinlearn/ns/quasi_engine/neuro_concept_structure.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+# A concept structure is convieved as the way how concepts/relations interacts with object/objects
+# and produce the probabilitic outcomes. Basically a concept structure is a set of functions that 
+# contains the way to calculate the following things:
+# Pr[c|e] which means the outcome prob of e entails c 
+# Pr[r|e1,e2] which means the prob of (e1,e1) entails r
+
+class NeuroConceptStructure(nn.Module):
+    def __init__(self):return 0
+
+    def PrConceptMeasure(value,entity): return 0
+
+    def PrRelationMeasure(relation,entity1,entity2): return 0
